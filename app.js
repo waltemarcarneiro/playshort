8f5acd6407e964bd89ac4741bd1f814a4d8d0ad5
Waltemar <78711339+WaltemarCarneiro@users.noreply.github.com>
2022-09-09 03:53:18 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 if('serviceWorker' in navigator){
-  navigator.serviceWorker.register('/sw.js')
+  navigator.serviceWorker.register('https://raw.githubusercontent.com/WaltemarCarneiro/playshort/main/sw.js')
   .then((reg) => console.log('service worker registered' reg))
   .catch((err) => console.log('service worker not registered', err));
   }
